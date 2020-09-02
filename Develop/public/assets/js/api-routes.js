e6c6a13cc4bb499851fb60cf089a884871569609
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-09-01 21:00:17 -0400

    updated server and files

--- Develop/public/assets/js/api-routes.js
+++ Develop/public/assets/js/api-routes.js
@@ -2,7 +2,7 @@ const fs = require ("fs");
 
 module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
-        fs.readFile("../../../db/db.json", (err, data) => {
+        fs.readFile("../db/db.json", (err, data) => { // FIX FILE PATH
             if (err) throw err;
             console.log(JSON.parse(data));
             res.json(JSON.parse(data));
